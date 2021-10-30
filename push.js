7b6c6876572a242799f2a91f9f9455d046b8fe61
Mkona055 <mkona055@uottawa.ca>
2021-10-30 16:19:24 -0400

    fix

--- push.js
+++ push.js
@@ -14,10 +14,10 @@ webpush.setVapidDetails(
 
 // This is the same output of calling JSON.stringify on a PushSubscription
 const pushSubscription = {
-    endpoint:"https://wns2-bl2p.notify.windows.com/w/?token=BQYAAADhDEEUuHDhF3c3Nu%2buVh9AY9zaEuzDW5GRFD7s21JqoDNq3ARZc2gaIohmhWVFa9SVlmx9X6LAYZtvenbZacp1eVgf3nZhVK%2b4VHyv7%2bJ5abDTWW1wOC64m5MMdhdyUyZ76AvkvBLyujGs%2f0%2bK8Php%2feBLHmU1dPV4lB7Fr7CUc2%2fYys6xh8ZtobSfQEkvIGeZmYaBdDv6v%2bCNk85mIj%2fR7oCYF3r9Wo1AlFQKp2bNUj%2bDpMasjgH6C1hMIjzn6mrJO5%2fCRjbSS0Z2829SBaw4Dp6vDDBfu7IU4reZLEBjGiMz01C2ikoCbmVI9uTYj2tpON9cPmc5hJ7lp7y9K%2fn5QKPn70gHFn62SdgfMmCQhQ%3d%3d",
-    expirationTime:null,
-    keys:{p256dh:"BIuApeW3MgspGxnCtkETXboikB9Pxoh2Ig1CGVlngp_7bB0BzdVsH4OYjYsXj1BFeflmduRBR8bByXqxQFELQTU",
-            auth:"4N9pvaSh4oYE6Ym6bkuprA"
+    endpoint:"https://fcm.googleapis.com/fcm/send/etEnSumsWlI:APA91bFqUef5Pz97TTMsQZ4MmlbQOC0eLU6m0YB54G7FaWUwcc9TAsX3g7D1ULJPeYAMgc0eaSFac54TvsDEqi8Iwa5slhERFWTVpAuPhOFRKrsXrocQUj8SMawJLcYYqFh4WE4DVL0N",
+    keys:{
+        auth:"3Z0ml-ui5w4Njas9iX492w",
+        p256dh:"GGY31IzlBVynfWWCu05d-LBBfX3RGIGuA7FRJTCkAIiRpR7MGLxBTTLdnG9_5IG8l8-z9LUeRPJgA-BNq9CpQE"
         }
 };
 
