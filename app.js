62d36177ea20e47da98bac9bf22e1deaea0172d4
Mkona055 <mkona055@uottawa.ca>
2021-11-25 20:15:37 -0500

    Fixing bugs where index.html is not seen by Netlify

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/pages'));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/pages/index.html");
+    res.sendFile(__dirname + "/index.html");
     
 });
-app.listen(3000, function () {
-    console.log("Server is running on localhost3000");
+app.listen(5000, function () {
+    console.log("Server is running on localhost5000");
 });
